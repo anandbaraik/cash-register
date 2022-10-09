e8e93005500646027f38ef3d989bc4dbb00e45d0
anandbaraik <anandbaraik2014@gmail.com>
2022-10-09 23:43:48 +0530

    improvement:validation for negative value added

--- assets/app.js
+++ assets/app.js
@@ -7,12 +7,15 @@ const availNotes = [2000, 500, 100, 20, 10, 5, 1];
 const errorDiv = document.querySelector('.error-msg');
 const nextBtn = document.querySelector('.next-btn');
 
-checkBtn.addEventListener('click', handleCheckBtn);
 nextBtn.addEventListener('click', handleNextBtn);
+checkBtn.addEventListener('click', handleCheckBtn);
 
 function handleNextBtn() {
+
     hideErorMsg();
     let billAmount = parseInt(billAmountInput.value);
+
+    //check if bill amount is valid & if valid show cash inout field
     if(!Number.isInteger(billAmount)) {
         showErorMsg("Enter valid bill amount. 😠😠");
     } else if(billAmount > 0) {
@@ -24,27 +27,34 @@ function handleNextBtn() {
     }
 }
 
-function validateBillAmount(billAmount) {
-    if(!Number.isInteger(billAmount)) showErorMsg("Enter valid bill amount. 😠😠");
-    if(billAmount <= 0) showErorMsg("Bill amount should be greater than 0 😠😠");
-}
-
-function validateCashGiven(cashGiven) {
-    if(!Number.isInteger(cashGiven)) showErorMsg("Enter valid cash given amount. 😠😠");
-    if(cashGiven <= 0) showErorMsg("Cash given amount should be greater than 0 😠😠");
-}
-
 function handleCheckBtn() {
+
     hideErorMsg();
     document.querySelector('.cash-change').style.display = "none";
+
     let billAmount = parseInt(billAmountInput.value);
     let cashGiven = parseInt(cashGivenInput.value);
 
-    validateBillAmount(billAmount);
-    validateCashGiven(cashGiven);
+    //check if bill amount is non negatve or not empty
+    if(!Number.isInteger(billAmount)){
+        showErorMsg("Enter valid bill amount. 😠😠");
+        return false;
+    } else if(billAmount <= 0) {
+        showErorMsg("Bill amount should be greater than 0 😠😠");
+        return false;
+    }
+
+    //check if cash given amount is non negatve or not empty
+    if(!Number.isInteger(cashGiven)){
+        showErorMsg("Enter valid cash given amount. 😠😠");
+        return false;
+    } else if(cashGiven <= 0) {
+        showErorMsg("Cash given amount should be greater than 0 😠😠");
+        return false;
+    }
 
     let cashToBeReturn = cashGiven - billAmount;
-
+    //check if cash given amount is less than bill amount
     if(
         (cashGiven > 0) && 
         (billAmount > 0) && 
@@ -52,9 +62,13 @@ function handleCheckBtn() {
     ){
         showErorMsg("Cash is less than bill amount! wanna wash the plates? 😀😀");
     }
+
+    //check if bill amount is equal to cash given
     if(cashGiven == billAmount) {
         showErorMsg("No amount to be returned.👍👍");
     }
+
+    //calculate change if cash given is greater than bill amount
     if(
         (cashGiven > 0) && 
         (billAmount > 0) &&
@@ -87,3 +101,8 @@ function calculateCashToBeReturn(amountToBeReturn) {
         amountToBeReturn = remainingAmount;
     }
 }
+
+//The Math.trunc() method returns the integer part of a number. ex: (0.3656 => 0)
+//The Math.trunc() method removes the decimals (does NOT round the number).
+//display = "inline-block" ==> Displays an element as an inline-level block container. itself is formatted as an inline element, but height and width values can be applied
+//display = "block" ==> Displays an element as a block element (like <p>). It starts on a new line, and takes up the whole width
